712948aacc967efce5301899767d382f91e72b04
Anna Volfman <aniya_07@ukr.net>
2023-04-02 20:39:58 +0300

    add express

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // native node.js :
 
-
 // const http = require("http");
 // const fs = require("fs").promises;
 
@@ -33,30 +32,39 @@
 //   console.log(`Server works at port: ${PORT}`);
 // });
 
-
 // express:
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 8081;
 
-app.get('/home', (req, res) => {
-  res.sendStatus(200);
-  // res.status(500).json({ javascript: 'object' });
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // для парсингу html форм
+app.use(express.static("public")); // для публічних директорій
+
+app.use((req, res, next) => {
+  // res.status(500).json({ javascript: "object" });
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+// app.get('/home', (req, res) => {
+//   res.sendStatus(200);
+//   // res.status(500).json({ javascript: 'object' });
+// })
 
 // app.use () для всіх видів відповідей
 
 app.post("/home", (req, res) => {
+  if (!req.body.test) {
+    return res.status(400).json({ status: "test is required" });
+  }
+  console.log(req.body);
   // res.status(500).json({ javascript: "object" });
-  res.sendStatus(200);
+  // res.sendStatus(200);
+  res.json({ javascript: "object", body: req.body });
 });
-
-app.use((req, res) => {
-  res.status(500).json({ javascript: "object" });
-});
-
 
 app.listen(PORT, (err) => {
   if (err) {
